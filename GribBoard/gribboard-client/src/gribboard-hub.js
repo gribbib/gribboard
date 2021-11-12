550df94672f8f5a16be7b3bce09ebe7cace8883d
gribbib <j.git@motschi.email>
2021-11-12 10:36:41 +0100

    Remove static container URL
    Belongs to #4

--- GribBoard/gribboard-client/src/gribboard-hub.js
+++ GribBoard/gribboard-client/src/gribboard-hub.js
@@ -4,7 +4,7 @@ import { HubConnectionBuilder, LogLevel } from '@aspnet/signalr'
 export default {
     install: (app, options) => {
         const connection = new HubConnectionBuilder()
-            .withUrl('https://localhost:5051/gribboardHub')
+            .withUrl('/gribboardHub')
             .configureLogging(LogLevel.Information)
             .build();
         app.config.globalProperties.$gribboardHub = connection
